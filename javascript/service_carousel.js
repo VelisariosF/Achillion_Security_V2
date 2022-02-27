24fd20ed20e12a6ac8524dff0cec5dc3de540007
Velisarios1234 <fafoutis@csd.auth.gr>
2022-02-27 17:54:27 +0200

    project updated

--- javascript/service_carousel.js
+++ javascript/service_carousel.js
@@ -1,6 +1,6 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
-
+const service_section_background = document.querySelector('service_container_section');
 const slider = document.querySelector('.slider');
 
 var index = 0;
@@ -9,7 +9,7 @@ var index = 0;
 left.addEventListener('click', function() {
   index = (index > 0) ? index - 1 : 0;
   slider.style.transform = 'translateX(' + (index) * -33.3333 + '%)';
-
+ 
 });
 
 right.addEventListener('click', function() {
